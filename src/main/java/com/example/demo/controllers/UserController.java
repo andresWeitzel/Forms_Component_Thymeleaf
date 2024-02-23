74b8dd68a81984e0227a105c619ca1a46ac092cf
Andrés Weitzel <andres96energy@hotmail.com>
2024-02-23 02:10:36 -0300

    Added login

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -2,13 +2,20 @@ package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
-	@GetMapping("/users")
+	@GetMapping("/")
 	public String index() {
 		return "users/index";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "users/login";
 	}
 
 }
